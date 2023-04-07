963c86b42f00494edcae5f6137b16153dd254b0d
yeongseoPark <1park4170@gmail.com>
2023-04-07 13:49:15 +0900

    coalescing 80/ random1,2 95/ binary 43/ binary2 36

--- mm.c
+++ mm.c
@@ -41,9 +41,7 @@ team_t team = {
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
-/* rounds up to the nearest multiple of ALIGNMENT 
-
-*/
+/* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 /* 
     - size 2 , ALIGNMENT 8 가정
@@ -68,7 +66,7 @@ team_t team = {
 
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 
-/* 크기와 할당비트 통합하여 헤더와 푸터에 저장할 수 있는 값 리턴 */
+/* 크기와 할당비트 통합하여 헤더와 푸터에 저장할 값 리턴 */
 #define PACK(size, alloc) ((size) | (alloc)) 
 /* 
     size 7, 할당비트 8 이면 
@@ -97,7 +95,7 @@ team_t team = {
 #define PUT(p, val)  (*(unsigned int *)(p)) = (val)
 /* 인자 p가 가리키는 워드에 val을 저장 */
 
-/* 주소 p에 있는 헤더 또는 푸터의 size를 리턴?
+/* 주소 p에 있는 헤더 또는 푸터의 size를 리턴
     : p에 0x19(10진수로 25)가 저장돼있다 치자,
     해당 연산은 & ~0x7을 통해 하위 3비트를 지워준다.
     그래서 25에 대해 GET_SIZE(25)를 하면, 결과로 0x18(10진수로 24)가 나오는데
@@ -144,6 +142,12 @@ team_t team = {
  */
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
 
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+
+void* heap_listp;
 
 /* 
  * mm_init - initialize the malloc package.
@@ -153,8 +157,84 @@ team_t team = {
  */
 int mm_init(void)
 {
+    if ((heap_listp = mem_sbrk(4 *WSIZE)) == (void *)-1) // 시스템에서 4워드를 가져옴, 문제생기면 -1리턴
+        return -1;
+
+    PUT(heap_listp, 0);  // 패딩
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); // 프롤로그 헤더 , 1001 저장(8사이즈, alloc알려주는 1비트)
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); // 프롤로그 푸터 , 1001 저장
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1)); // 에필로그 헤더,  0001 저장(0사이즈, alloc알려주는 1비트)
+    heap_listp += (2*WSIZE); // 프롤로그 푸터위치로 이동
+
+    /* 힙을  */
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+
     return 0;
 }
+
+/* 
+    1. 힙이 초기화될 때
+    2. mm_malloc이 적당한 맞춤 fit을 찾지 못했을때
+    -> 두가지 경우에서 호출됨
+*/
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    /* 요청한 크기를 정렬을 유지하기 위해 인접 2워드의 배수(8바이트)로 반올림 */
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1) // 메모리 시스템에게 추가적인 힙 공간 요청
+        return NULL; // 예외처리
+    
+    /* free block의 헤더/푸터 , 에필로그 헤더를 초기화 */
+    PUT(HDRP(bp), PACK(size, 0)); // 새 가용 블록의 헤더
+    PUT(FTRP(bp), PACK(size, 0)); // 새 가용 블록의 푸터
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // 새 에필로그 블록 헤더
+
+    /* 이전 블록이 free라면 두개의 가용 블록을 통합후 통합된 블록의 블록 포인터 리턴  */
+    return coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    /* 이전과 다음 블록 모두 할당된 경우 */
+    if (prev_alloc && next_alloc) { 
+        return bp;
+    }
+
+    /* 이전 블록 할당, 다음 블록은 free */
+    else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    /* 이전 블록은 free, 다음 블록은 할당됨 */
+    else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0)); // 이전 블록의 블록포인터에 통합된 사이즈를 써줘야함
+        bp = PREV_BLKP(bp); // 통합됐으니 bp도 이전 블록의 블록포인터로 변경
+    }
+    
+    /* 이전과 다음 블록 모두 free */
+    else {
+        size += GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    return bp;
+}
+
 
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
@@ -162,17 +242,72 @@ int mm_init(void)
  * : 최소 size 바이트의 할당된 블록에 대한 포인터를 리턴
  *  - 전체 할당된 블록은 힙 영역에 있어야 하고, 다른 할당된 chunk와 겹치면 안됨
  *  - 8 바이트의 aligned된 포인터 리턴?
- * 
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
+    size_t asize; // adjusted size
+    size_t extendsize;
+    char *bp;
+
+    if (size == 0)
+        return NULL;
+
+    /* 정렬 요건과 오버헤드를 포함할 수 있도록 블록 사이즈를 조정 */
+    if (size <= DSIZE)
+        /* 최소 16바이트 : 8바이트는 정렬 요건을 위해, 8바이트는 헤더와 푸터 */
+        asize = 2 * DSIZE; 
+
+    else // 8바이트 넘는 요청
+        /* 오버헤드 바이트를 추가하고, 가장 가까운 8의 배수로 올림 
+            - DSIZE - 1을 더해주지 않는다면, 반올림이 되버림
+        */
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+    
+    if ((bp = find_fit(asize)) != NULL) { // 가용 블록을 가용 리스트에서 검색, 적절한 공간 찾음
+        place(bp, asize);
+        return bp;
+    } 
+
+    /* 적절한 fit을 찾지 못했음 - 메모리 요청 더하고 블록 위치시킴 */
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL) 
+        return NULL;
+    place(bp, asize);
+    return bp;
+}
+
+/* First - Fit */
+static void *find_fit(size_t asize) 
+{
+    void *bp;
+
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) { // 에필로그(사이즈 0)만나기 전까지 
+        /* 할당되지 않았고, 해당 블록의 사이즈가 더 크거나 같다면 리턴  */
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+
+    return NULL; // NO fit
+}
+
+/* bp포인터가 할당받을 공간을 가리키는 포인터? */
+static void place(void *bp, size_t asize)
+{
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    /* 최소 블록의 크기는 16바이트 */
+    if ((csize - asize) >= (2*DSIZE)) { // 현재 블록사이즈 - 할당받을 사이즈가 16이상이면, 해당 블록을 나눌 수 있음
+        PUT(HDRP(bp), PACK(asize , 1)); // 첫번째 블록에 asize만큼의 공간 할당
+        PUT(FTRP(bp), PACK(asize , 1));
+        bp = NEXT_BLKP(bp); // 두번째 블록에는 나머지 공간(이거는 가용한 공간)
+        PUT(HDRP(bp), PACK(csize - asize , 0));
+        PUT(FTRP(bp), PACK(csize - asize , 0));
+    }
+    /* 해당 블록을 나눌 수 없음 */
     else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
     }
 }
 
@@ -181,8 +316,13 @@ void *mm_malloc(size_t size)
  : ptr이 가리키고 있는 블럭을 free, 아무것도 반환하지 않음
     - mm_malloc이나 mm_realloc호출을 통해 받았고, 아직 free 되지 않은 포인터가 넘겨졌을때만 작동
  */
-void mm_free(void *ptr)
+void mm_free(void *bp)
 {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 /*
